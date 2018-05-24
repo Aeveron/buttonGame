02932897d2b5aa7715977272f90eb14bc4b4c51e
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-05-24 11:49:37 +0200

    x
    x

---
+++ buttonGame.js
@@ -0,0 +1,11 @@
+var btnRed = document.getElementById('btnRed');
+var btnOrange = document.getElementById('btnOrange');
+var btnYellow = document.getElementById('btnYellow');
+var btnGreen = document.getElementById('btnGreen');
+var btnBlue = document.getElementById('btnBlue');
+var btnIndigo = document.getElementById('btnIndigo');
+var btnViolet = document.getElementById('btnViolet');
+
+function colorClick() {
+    
+}
