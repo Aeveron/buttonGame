6c1f0a8f3bc4dc0d99b2b64ce40c262889d40235
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-05-24 13:24:27 +0200

    x
    xx

--- buttonGame.js
+++ buttonGame.js
@@ -5,9 +5,25 @@ var btnGreen = document.getElementById('btnGreen');
 var btnBlue = document.getElementById('btnBlue');
 var btnIndigo = document.getElementById('btnIndigo');
 var btnViolet = document.getElementById('btnViolet');
+var secDiv = document.getElementById('secDiv');
+
 
 function colorClick(clicked) {
     if (btnRed == clicked) {
-        btnRed.style.color = 'red';
+        btnRed.style.backgroundColor = 'red';       
     }
+    
+}
+
+function startGame(clicky) {
+    if (clicky == clicky) {
+        btnRed.style.backgroundColor = 'lightgrey';
+        btnOrange.style.backgroundColor = 'lightgrey';
+        btnYellow.style.backgroundColor = 'lightgrey';
+        btnGreen.style.backgroundColor = 'lightgrey';
+        btnBlue.style.backgroundColor = 'lightgrey';
+        btnIndigo.style.backgroundColor = 'lightgrey';
+        btnViolet.style.backgroundColor = 'lightgrey';
+    }
+
 }
